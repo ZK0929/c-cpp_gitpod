1d46a2e6aa79c4f433b95d2923cd199eeb9bbf63
ZHIKIN0929 <104434654+ZHIKIN0929@users.noreply.github.com>
2022-04-26 21:29:25 +0800

    Update ex1-1.cpp

--- ex1-1.cpp
+++ ex1-1.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout<<"Hello World";
-
-    return 0;
+#include<stdio.h>
+int main (void) {
+     printf(" hello Elaina "); 
+     printf(" my height is %d m %d cm \n",2,10);
 }
