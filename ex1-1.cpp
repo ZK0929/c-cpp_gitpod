9ef00f634c2d583c618cafc798c13c919e85da07
ZHIKIN0929 <104434654+ZHIKIN0929@users.noreply.github.com>
2022-05-01 09:39:33 +0800

    Update ex1-1.cpp

--- ex1-1.cpp
+++ ex1-1.cpp
@@ -1,5 +1,11 @@
-#include<stdio.h>
-int main (void) {
-     printf(" hello Elaina "); 
-     printf(" my height is %d m %d cm \n",2,10);
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+    printf("Hello World\n"); //the first program
+    cout<<"Hello World\n";  /*the first c++ program */
+    std::cout<<"Hello World";
+    return 0;
 }
