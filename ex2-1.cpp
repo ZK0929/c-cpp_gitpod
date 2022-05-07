6786aca021761f9047bb62194dc358cd8888e4b4
ZK0929 <95951938+ZK0929@users.noreply.github.com>
2022-05-07 12:13:16 +0800

    Create ex2-1.cpp

---
+++ ex2-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    double f;
+    char s;
+    
+    printf("Please input a integer \n");
+    scanf("%d",&i);
+    printf("Your input integer = %d\n",i);
+    return 0;
+}
